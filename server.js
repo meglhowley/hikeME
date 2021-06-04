55e9e2d0cdcfe017a7a5e265a1f72afa3220a2f7
Megan Howley <meglhowley@gmail.com>
2021-06-04 17:58:26 -0400

    creating app

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const path = require('path')
 
 const PORT = process.env.PORT || 3001
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
 const app = express()
 
 app.use(bodyParser.json())
